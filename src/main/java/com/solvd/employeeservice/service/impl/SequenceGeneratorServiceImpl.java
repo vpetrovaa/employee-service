95f158cb2f5c16dca5ab6f4e368fbe4b88f3cb48
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-22 17:53:03 +0300

    Unwrapping Mono without blocking was configured

--- src/main/java/com/solvd/employeeservice/service/impl/SequenceGeneratorServiceImpl.java
+++ src/main/java/com/solvd/employeeservice/service/impl/SequenceGeneratorServiceImpl.java
@@ -3,12 +3,10 @@ package com.solvd.employeeservice.service.impl;
 import com.solvd.employeeservice.domain.DatabaseSequence;
 import com.solvd.employeeservice.service.SequenceGeneratorService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
@@ -20,12 +18,12 @@ public class SequenceGeneratorServiceImpl implements SequenceGeneratorService {
 
     private final ReactiveMongoOperations mongoOperations;
 
+    @SneakyThrows
     @Override
     public Long generateSequence(String sequenceName) {
-        Mono<DatabaseSequence> counter = mongoOperations.findAndModify(query(where("_id").is(sequenceName)),
+        return mongoOperations.findAndModify(query(where("_id").is(sequenceName)),
                 new Update().inc("sequence",1), options().returnNew(true).upsert(true),
-                DatabaseSequence.class);
-        return !Objects.isNull(counter) ? counter.block().getSequence() : 1;
+                DatabaseSequence.class).toFuture().get().getSequence();
     }
 
 }
