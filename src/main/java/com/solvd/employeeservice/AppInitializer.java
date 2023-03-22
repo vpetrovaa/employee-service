f5940cdc2df45e661c60d4f5c11efef21e1caf9d
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-22 19:39:02 +0300

    MongoDb configuration annotation was added

--- src/main/java/com/solvd/employeeservice/AppInitializer.java
+++ src/main/java/com/solvd/employeeservice/AppInitializer.java
@@ -1,9 +1,12 @@
 package com.solvd.employeeservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class AppInitializer {
 
 	public static void main(String[] args) {
