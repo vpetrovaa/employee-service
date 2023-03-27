ae05f1265ea77aefb877de6cd0862f66c49b1a09
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-27 11:22:50 +0300

    #19

--- src/main/java/com/solvd/employeeservice/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/solvd/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -5,22 +5,29 @@ import com.solvd.employeeservice.domain.exception.ResourceAlreadyExistsException
 import com.solvd.employeeservice.repository.EmployeeRepository;
 import com.solvd.employeeservice.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
     @Override
+    @Cacheable(key = "#id", value = "employees")
     public Mono<Employee> findById(String id) {
+        log.info("Method was called");
         return employeeRepository.findById(id);
     }
 
     @Override
+    @CachePut(key = "#employee.id", value = "employees")
     public Mono<Employee> create(Employee employee) {
         return employeeRepository.existsByEmail(employee.getEmail())
                 .flatMap(isExist -> {
